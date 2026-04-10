18a6b52ee002920024fa95db6886ec602608cd6e
Priyanka23 <245123748302@mvsrec.edu.in>
2026-04-10 13:12:22 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Variables are initialized with fixed values instead of using scanf
+    int x = 10;
+    int y = 25;
+    int sum = x + y;
+
+    // Display the result
+    printf("The sum of %d and %d is: %d\n", x, y, sum);
+    
+    return 0;
+}
+
